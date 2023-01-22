ab3fe8683ed8d6636908dd6bff87fd4bba5534a5
ArtemBohak <abogak3@gmail.com>
2023-01-22 22:33:13 +0200

    Created new components, upgraded styling. * Greeting component added. * Card component created for styling other components. * Small fixes of styling the background image.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Components/Header/Header'
+import Greeting from './Components/Greeting/Greeting'
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Greeting />
     </React.Fragment>
   );
 }
